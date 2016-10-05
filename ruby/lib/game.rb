d1041459b8344ac66279e6effb59814ca8684320
Chuck Lauer Vose <vosechu@gmail.com>
2016-10-04 21:01:31 -0700

    initial attempt by end of event

--- ruby/lib/game.rb
+++ ruby/lib/game.rb
@@ -1,3 +1,53 @@
 class Game
+  def initialize(input:)
+    @state = parse_state(input)
+  end
 
+  def displayed_state
+    @state
+  end
+
+  # Starts from 0,0 at upper left
+  def at(x,y)
+    @state[y][x] rescue " "
+  end
+
+  def tick
+    new_state = @state.map(&:dup)
+
+    @state.each_with_index do |row, y|
+      x = 0
+      row.each_char do |_cell|
+        new_state[y][x] = apply_rules(x, y)
+        x += 1
+      end
+    end
+  end
+
+  # Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+  # Any live cell with two or three live neighbours lives on to the next generation.
+  # Any live cell with more than three live neighbours dies, as if by over-population.
+  # Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+  def apply_rules(x, y)
+    neighbors = [
+      at(x-1, y-1), at(x, y-1), at(x+1, y-1),
+      at(x-1, y),               at(x+1, y),
+      at(x-1, y+1), at(x, y+1), at(x+1, y+1) ]
+
+    num_alive = neighbors.select {|n| n == "X"}.count
+    puts num_alive
+    puts neighbors.to_s
+
+    if num_alive < 2 || num_alive > 3
+      return " "
+    elsif num_alive == 3 || (at(x,y) == " " && num_alive == 3)
+      return "X"
+    end
+  end
+
+  private
+
+  def parse_state(input)
+    input.split("\n")
+  end
 end
--- ruby/spec/lib/game_spec.rb
+++ ruby/spec/lib/game_spec.rb
@@ -3,5 +3,64 @@ require 'game'
 
 describe Game do
   it 'exists' do
+    # 100x100 grid
+    input = " X \nXX \n X "
+    game = Game.new(input: input)
+
+    expected_output = ["XXX", "   ", " X "]
+
+    expect { game.tick }.to change {
+      game.displayed_state
+    }.from([" X ", "XX ", " X "]).to(expected_output)
+  end
+
+  describe 'the rules' do
+    # Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+    it 'satisfies rule 1' do
+      input = "   \n X \n   "
+      game = Game.new(input: input)
+
+      expect { game.tick }
+        .to change { game.at(1,1) }
+        .from("X")
+        .to(" ")
+    end
+
+    # Any live cell with two or three live neighbours lives on to the next generation.
+    it 'satisfies rule 2' do
+      input = " X \n X \n X "
+      game = Game.new(input: input)
+
+      expect { game.tick }
+        .to change { game.at(1,1) }
+        .from("X")
+        .to("X")
+    end
+
+    # Any live cell with more than three live neighbours dies, as if by over-population.
+    it 'satisfies rule 3' do
+      input = " X \nXX \n X "
+      game = Game.new(input: input)
+
+      expect { game.tick }
+        .to change { game.at(1,1) }
+        .from("X")
+        .to(" ")
+    end
+
+    # Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+    it 'satisfies rule 4' do
+      input = " X \nX  \n X "
+      game = Game.new(input: input)
+
+      expect { game.tick }
+        .to change { game.at(1,1) }
+        .from(" ")
+        .to("X")
+    end
+  end
+
+  describe 'stable shapes' do
+
   end
 end
